0cd9173673c1e1ea6f658b05d333c1844543a571
Shivam <shivam123ortan@gmail.com>
2022-08-07 16:32:55 +0530

    Edited

--- 2_ReverseTheArray/Reverse.java
+++ 2_ReverseTheArray/Reverse.java
@@ -1,13 +1,26 @@
+/*Given an array (or string), the task is to reverse the array/string.
+Examples : 
+ 
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4} */
+
 public class Reverse {
 
     static void reverse(int arr[], int n){
         int l[] = new int[n];
+
+        //Reversing
         for (int i=0; i<n; i++){
             l[i] = arr[n-i-1];
         }
-        // System.out.println(l[]);
+        
+        //Output
         for (int i=0; i<n; i++){
-            System.out.println(l[i]);
+            System.out.print(l[i] + " ");
         }
     }
     
--- 4_ContainsDublicate/ConDub.java
+++ 4_ContainsDublicate/ConDub.java
@@ -1,3 +1,22 @@
+/*
+Given an integer array nums, return true if any 
+value appears at least twice in the array, and 
+return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ */
+
 import java.util.*;
 public class ConDub {
     static boolean containDouble(int arr[]){
@@ -17,6 +36,7 @@ public class ConDub {
         for (int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+        sc.close();
         System.out.println(containDouble(arr));
     }    
 }
